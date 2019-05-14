d64981b4b1723f8f70eb24b141108efa4d104809
Lovely Fellow <44167585+Lovely-Fellow@users.noreply.github.com>
2019-05-14 10:26:34 +0200

    Add Hellow World Page

---
+++ upwork_pardeep/src/Dashboard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class DashBoard extends Component {
+
+  
+  render() {
+      
+    return (
+      <div>
+          Hellow World
+      </div>
+        
+      
+    );
+    
+  }
+  
+}
+
+export default DashBoard;
